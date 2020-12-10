11da56298e7c35bc5d14fabba214b7852be4ba51
MohammadSyahlan <75625226+MohammadSyahlan@users.noreply.github.com>
2020-12-10 18:56:51 +0700

    Add files via upload

---
+++ data/KantorCamat_4.js
@@ -0,0 +1,21 @@
+var json_KantorCamat_4 = {
+"type": "FeatureCollection",
+"name": "KantorCamat_4",
+"features": [
+{ "type": "Feature", "properties": { "ID": 0.0, "KETERANGAN": "Ibukota Kabupaten" }, "geometry": { "type": "Point", "coordinates": [ 72035.763707424892345, 758630.551376464543864 ] } },
+{ "type": "Feature", "properties": { "ID": 0.0, "KETERANGAN": "Ibukota Kecamatan" }, "geometry": { "type": "Point", "coordinates": [ 61538.475310718989931, 758517.790188577724621 ] } },
+{ "type": "Feature", "properties": { "ID": 0.0, "KETERANGAN": "Ibukota Kecamatan" }, "geometry": { "type": "Point", "coordinates": [ 62803.236893674446037, 748310.726471803267486 ] } },
+{ "type": "Feature", "properties": { "ID": 0.0, "KETERANGAN": "Ibukota Kecamatan" }, "geometry": { "type": "Point", "coordinates": [ 72614.669212787863216, 756888.136963879456744 ] } },
+{ "type": "Feature", "properties": { "ID": 0.0, "KETERANGAN": "Ibukota Kecamatan" }, "geometry": { "type": "Point", "coordinates": [ 76430.061957670550328, 747320.443766972748563 ] } },
+{ "type": "Feature", "properties": { "ID": 0.0, "KETERANGAN": "Ibukota Kecamatan" }, "geometry": { "type": "Point", "coordinates": [ 78714.215452513497439, 741419.344235449330881 ] } },
+{ "type": "Feature", "properties": { "ID": 0.0, "KETERANGAN": "Ibukota Kecamatan" }, "geometry": { "type": "Point", "coordinates": [ 84077.052360443136422, 760106.719500045874156 ] } },
+{ "type": "Feature", "properties": { "ID": 0.0, "KETERANGAN": "Ibukota Kecamatan" }, "geometry": { "type": "Point", "coordinates": [ 86180.738192946344498, 756814.978737750672735 ] } },
+{ "type": "Feature", "properties": { "ID": 0.0, "KETERANGAN": "Ibukota Kecamatan" }, "geometry": { "type": "Point", "coordinates": [ 88459.174315959608066, 752663.451435288530774 ] } },
+{ "type": "Feature", "properties": { "ID": 0.0, "KETERANGAN": "Ibukota Kecamatan" }, "geometry": { "type": "Point", "coordinates": [ 85377.683127400930971, 747520.285194940632209 ] } },
+{ "type": "Feature", "properties": { "ID": 0.0, "KETERANGAN": "Ibukota Kecamatan" }, "geometry": { "type": "Point", "coordinates": [ 96980.7850293560914, 751370.501387141877785 ] } },
+{ "type": "Feature", "properties": { "ID": 0.0, "KETERANGAN": "Ibukota Kecamatan" }, "geometry": { "type": "Point", "coordinates": [ 101056.44422186385782, 741059.039952432969585 ] } },
+{ "type": "Feature", "properties": { "ID": 0.0, "KETERANGAN": "Ibukota Kecamatan" }, "geometry": { "type": "Point", "coordinates": [ 106486.113538839068497, 756475.710439277114347 ] } },
+{ "type": "Feature", "properties": { "ID": 0.0, "KETERANGAN": "Ibukota Kecamatan" }, "geometry": { "type": "Point", "coordinates": [ 101858.20173627960321, 760017.56960635969881 ] } },
+{ "type": "Feature", "properties": { "ID": 0.0, "KETERANGAN": "Ibukota Kecamatan" }, "geometry": { "type": "Point", "coordinates": [ 89736.12519289937336, 766519.446602072101086 ] } }
+]
+}
